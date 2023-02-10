1d6dd6524705257d8ae2fdc8830b1937561f2403
jiheon788 <jiheon788@ajou.ac.kr>
2023-02-10 16:38:35 +0900

    feat: 네비게이션 바

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from '@/components/Nav';
 import Router from './Router';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Router />
     </div>
   );

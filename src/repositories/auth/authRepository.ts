0d45db29efb97ae8ccf606d816dc8539fe465123
jiheon788 <jiheon788@ajou.ac.kr>
2023-02-10 21:02:29 +0900

    style: 로그인, 회원가입 repository param 변경, 통일성 주기 위함

--- src/repositories/auth/authRepository.ts
+++ src/repositories/auth/authRepository.ts
@@ -1,18 +1,24 @@
 import apiClient from '@/lib/apiClient';
 import { postSignParam } from './authRepository.param';
 
-export const postSignUp = async (data: postSignParam) => {
+export const postSignUp = async ({ email, password }: postSignParam) => {
   return await apiClient({
     method: 'post',
     url: '/auth/signup',
-    data,
+    data: {
+      email,
+      password,
+    },
   });
 };
 
-export const postSignIn = async (data: postSignParam) => {
+export const postSignIn = async ({ email, password }: postSignParam) => {
   return await apiClient({
     method: 'post',
     url: '/auth/signin',
-    data,
+    data: {
+      email,
+      password,
+    },
   });
 };
